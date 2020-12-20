49c7f26a49f5ec2d9f395c30e33ba4888dedd2bc
shravan20 <mrshravankumarb@gmail.com>
2020-12-20 21:22:46 +0530

    [ js added ] feature | JS Added Updated for index.html

--- js/index.js
+++ js/index.js
@@ -1,24 +1,86 @@
+let about = '\n<ul><li>Skilled Backend Developer with the ability to learn and implement new technologies quickly.</li>' + 
+'\n<li>Worked on IoT technology and Web Technologies( MEAN / MERN )</li>' +
+'\n<li>Currently working and exploring React with Redux Architecture and Express Framework, along with TypeScript.</li>' +
+'</ul>';
+
+
+let projects = `
+<style>
+table, th, td {
+  border: 1px solid red;
+  border-collapse: collapse;
+}
+</style>
+<br>
+<table style="width:80%">
+  <tr>
+    <th>Project Name</th>
+    <th>Description</th> 
+  </tr>
+  <tr>
+    <td>Obstacle Detection and Avoidance Robot</td>
+    <td>Mini project on Embedded Systems for RRCE - CSE Dept Level Project Expo, exhibiting an Obstacle Detection and Avoidance Robot using Arduino Uno and Ultrasonic Sensors.</td>
+  </tr>
+  <tr>
+    <td>Zoo Management Information System</td>
+    <td>The DBMS project for academic purpose was completed using Bootstrap as front end technology and PHP as backend technology with MySQL as database. This project used open-source cross-platform web server solution stack package tool namely XAMPP.</td>
+  </tr>
+  <tr>
+    <td>Online Food Portal Website</td>
+    <td>This Web Development project was completed to fulfill the requirement of the internship. The project was done using Bootstrap, AngularJS and jQuery as front end and using Expressjs Framework as backend and MongoDB as database. (MEAN Stack)</td>
+  </tr>
+  <tr>
+    <td>Smart Garbage Van Tracking and Alert System using IoT</td>
+    <td>descriptionThe project was completed using Raspberry Pi platform, and a MIT android application was created to notify the truck driver about the location of the Dustbin which is filled with amount of Garbage filled.</td>
+  </tr>
+  <tr>
+    <td>Internal E Banking Website</td>
+    <td>This was an Internal E Banking System project is a web application where you can transfer money to other users and can have a close watch on all your transactions.
+	Tech Stack: Bootstrap Library for the front-end dev, Backend using PHP and MySQL as the database.</td>
+  </tr>
+  <tr>
+    <td>Autonomous Garbage Collecting System using Internet of Things</td>
+    <td>The project aimed at designing a Garbage Collection System using a Robot in the Indoors based on Indoor Positioning System using Internet of Things. A smart bin was deployed which would notify the Collector Robot to collect the garbage as and when it was filled.</td>
+  </tr>
+  <tr>
+    <td>Animated Dynamic Quote for GitHub Profile README</td>
+    <td>An Open-Source Project, GitHub Dynamic Quote Generator simply allows you to add an image link in the markdown (README.md) and it will view you a dynamic quote for every user visiting your GitHub account.</td>
+  </tr>
+</table>
+<br>`;
+
+let contact = `
+
+│        Medium:  https://dev.to/imshravan                                           │
+│       Twitter:  https://twitter.com/imshravankb                                    │
+│        GitHub:  https://github.com/shravan20                                       │
+│      LinkedIn:  https://linkedin.com/in/shravankb                                  │
+│           Web:  https://ohmyscript.com 											 │
+│          Mail:  shravan@ohmyscript.com 											 │
+`
+
+
+
+
+
 $('body').terminal({
 	iam: function (name) {
-		this.echo('Hello, ' + name +
-			'. Welcome to GeeksForGeeks');
+		this.echo('Hello, ' + name + '. Welcome to GeeksForGeeks');
 	},
-	founder: function () {
-		this.echo('Sandeep Jain');
+	contact: function () {
+		this.echo(contact);
+	},
+	projects: function () {
+		this.echo( projects, {raw:true});
 	},
 	aboutme: function () {
-		this.echo('\n<ul><li>Skilled Backend Developer with the ability to learn and implement new technologies quickly.</li>' + 
-		'\n\n<li>Worked on IoT technology and Web Technologies( MEAN / MERN )</li>' +
-		'\n<li>Currently working and exploring React with Redux Architecture and Express Framework, along with TypeScript.</li>' +
-		'</ul>',
-		{raw:true}		
-		);
+		this.echo(about, {raw:true});
 	},
-	more: function () {
+	help: function () {
 		this.echo('\n These are commands available: \n');
 		this.echo('aboutme - briefs about the author of OhMyScript');
 		this.echo('projects - briefs about the projects worked on');
-		this.echo('contact - provides you contact details');
+		this.echo('contact - provides my contact details');
 		this.echo('blog - click <a href="https://ohmyscript.com/" target="__blank">ohmyscript</a>',{raw:true});
 		this.echo('\n');		
 	},
@@ -27,5 +89,5 @@ $('body').terminal({
 },
 {
     autocompleteMenu: true,
-    completion: ['foo', 'bar', 'baz']
+    completion: ['aboutme', 'projects', 'contact']
 });
